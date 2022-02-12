21cedcffb1ef9dff475c49e4a754d231bcb28747
Maximilian Füsslin <mfuesslin@gmail.com>
2022-02-12 22:58:19 +0100

    updated custom keyboard config

--- keyboards/gmmk/pro/iso/keymaps/koseduhemak/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/koseduhemak/keymap.c
@@ -161,6 +161,18 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
 }
 
+// IDLE TIMEOUTS
+#ifdef IDLE_TIMEOUT_ENABLE
+    #define TIMEOUT_THRESHOLD_DEFAULT   1    // default timeout minutes
+    #define TIMEOUT_THRESHOLD_MAX       15  // upper limits (2 hours and 10 minutes -- no rgb indicators above this value)
+
+    //prototype  functions
+    uint16_t get_timeout_threshold(void);
+    void timeout_reset_timer(void);
+    void timeout_update_threshold(bool increase);
+    void timeout_tick_timer(void);
+#endif  //IDLE_TIMEOUT_ENABLE
+
 // TIMEOUTS
 #ifdef IDLE_TIMEOUT_ENABLE
 static uint16_t timeout_timer     = 0;
