215459f31df03db15b2feb86be0878ea32d58b64
Maximilian Füsslin <mfuesslin@gmail.com>
2022-02-12 22:56:34 +0100

    updated custom keyboard config

--- keyboards/gmmk/pro/iso/keymaps/koseduhemak/config.h
+++ keyboards/gmmk/pro/iso/keymaps/koseduhemak/config.h
@@ -1,5 +1,5 @@
 // IDLE TIMEOUTS
-#ifdef IDLE_TIMEOUT_ENABLE
+/*#ifdef IDLE_TIMEOUT_ENABLE
     #define TIMEOUT_THRESHOLD_DEFAULT   5    // default timeout minutes
     #define TIMEOUT_THRESHOLD_MAX       140  // upper limits (2 hours and 10 minutes -- no rgb indicators above this value)
 
@@ -8,4 +8,4 @@
     void timeout_reset_timer(void);
     void timeout_update_threshold(bool increase);
     void timeout_tick_timer(void);
-#endif  //IDLE_TIMEOUT_ENABLE
+#endif  //IDLE_TIMEOUT_ENABLE*/
