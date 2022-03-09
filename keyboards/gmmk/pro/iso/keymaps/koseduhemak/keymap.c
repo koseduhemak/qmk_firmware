e0187886b2e9aa665bc6b04a3430c319f5503b22
Maximilian Füsslin <mfuesslin@gmail.com>
2022-03-09 17:10:45 +0100

    fixed play/pause key on mac

--- keyboards/gmmk/pro/iso/keymaps/koseduhemak/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/koseduhemak/keymap.c
@@ -18,14 +18,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "rgb_matrix_map.h"
 #include "led.c"
 
-enum layers {
-  WIN,
-  MAC,
-  MODS
-};
+enum layers { WIN, MAC, MODS };
 
 enum custom_keycodes {
-    ENCODERKNOB = SAFE_RANGE,
+#ifdef VIA_ENABLE
+    KC_ENCODERKNOB = USER00,
+#else
+    KC_ENCODERKNOB = SAFE_RANGE,
+#endif
+    KC_MISSION_CONTROL,
+    KC_LAUNCHPAD
 };
 
 // clang-format off
@@ -51,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [WIN] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TG(MAC),          ENCODERKNOB,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TG(MAC),          KC_ENCODERKNOB,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
         LT(MODS, KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
@@ -60,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [MAC] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TG(MAC),          ENCODERKNOB,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  TG(MAC),          KC_ENCODERKNOB,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
         LT(MODS, KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
@@ -69,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [MODS] = LAYOUT(
-        _______, KC_BRID, KC_BRIU, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, ENCODERKNOB, KC_VOLD, KC_VOLU, KC_WFAV, _______,          _______,
+        _______, KC_BRID, KC_BRIU, KC_MISSION_CONTROL, KC_LAUNCHPAD, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_WFAV, _______,          _______,
         _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,            _______,
         _______, _______, KC_W, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______, RGB_VAI,                   _______,
         _______, _______, KC_S, _______, _______, _______, _______, KC_LEFT,    KC_DOWN,    KC_RIGHT,    _______, _______, _______, _______,          _______,
@@ -83,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
     uint8_t mods_state = get_mods();
-    if (mods_state & MOD_BIT(KC_LCTL)) {  // If you are holding L shift, encoder changes layers
+    if (mods_state & MOD_BIT(KC_LCTL)) { // If you are holding L shift, encoder changes layers
         unregister_mods(MOD_BIT(KC_LCTL));
 
         if (clockwise)
@@ -92,14 +94,14 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_MPRV);
 
         register_mods(MOD_BIT(KC_LCTL));
-    } else if (mods_state & MOD_BIT(KC_RSFT)) {  // If you are holding R shift, Page up/dn
+    } else if (mods_state & MOD_BIT(KC_RSFT)) { // If you are holding R shift, Page up/dn
         unregister_mods(MOD_BIT(KC_RSFT));
         if (clockwise)
             tap_code16(KC_PGUP);
         else
             tap_code16(KC_PGDN);
         register_mods(MOD_BIT(KC_RSFT));
-    } else if (mods_state & MOD_BIT(KC_LGUI)) {  // if holding Left Shift, navigate next/prev word
+    } else if (mods_state & MOD_BIT(KC_LGUI)) { // if holding Left Shift, navigate next/prev word
         unregister_mods(MOD_BIT(KC_LGUI));
 
         if (clockwise)
@@ -108,12 +110,12 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code16(LCTL(KC_LEFT));
 
         register_mods(MOD_BIT(KC_LGUI));
-    } else if (mods_state & MOD_BIT(KC_RCTL)) {  // if holding Right Ctrl, change rgb hue/colour
+    } else if (mods_state & MOD_BIT(KC_RCTL)) { // if holding Right Ctrl, change rgb hue/colour
         if (clockwise)
             rgblight_increase_hue();
         else
             rgblight_decrease_hue();
-    } else if (mods_state & MOD_BIT(KC_LSFT)) {  // if holding Left Alt, change brightness up and down
+    } else if (mods_state & MOD_BIT(KC_LSFT)) { // if holding Left Alt, change brightness up and down
         unregister_mods(MOD_BIT(KC_LSFT));
         if (clockwise) {
             tap_code(KC_BRIU);
@@ -129,7 +131,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
-        }  // Otherwise it just changes volume
+        } // Otherwise it just changes volume
     }
     return true;
 }
@@ -142,16 +144,55 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case WIN:
         case MAC:
             switch (keycode) {
-                case ENCODERKNOB:
+                case KC_ENCODERKNOB:
                     if (mods_state & MOD_BIT(KC_LCTL)) {
                         if (record->event.pressed) {
+                            unregister_mods(MOD_BIT(KC_LCTL));
                             tap_code(KC_MPLY);
+                            register_mods(MOD_BIT(KC_LCTL));
+                        } else {
+                            host_consumer_send(0);
                         }
+                        return false;
+                    } else {
+                        if (record->event.pressed) {
+                            tap_code(KC_MUTE);
+                        } else {
+                            host_consumer_send(0);
+                        }
+                        return false;
                     }
+                    break;
+                case KC_BSPC:
+                    if (mods_state & MOD_BIT(KC_LCTL)) {
+                        if (record->event.pressed) {
+                            unregister_mods(MOD_BIT(KC_LCTL));
+                            tap_code(KC_DEL);
+                            register_mods(MOD_BIT(KC_LCTL));
+                        } else {
+                            host_consumer_send(0);
+                        }
+                        return false;
+                    }
+                    break;
             }
             break;
         case MODS:
             switch (keycode) {
+                case KC_MISSION_CONTROL:
+                    if (record->event.pressed) {
+                        host_consumer_send(0x29F);
+                    } else {
+                        host_consumer_send(0);
+                    }
+                    return false; // Skip all further processing of this key
+                case KC_LAUNCHPAD:
+                    if (record->event.pressed) {
+                        host_consumer_send(0x2A0);
+                    } else {
+                        host_consumer_send(0);
+                    }
+                    return false; // Skip all further processing of this key
                 case KC_C:
                     if (record->event.pressed) {
                         rgblight_sethsv_noeeprom(HSV_GOLD);
@@ -162,7 +203,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                         rgblight_sethsv_noeeprom(HSV_WHITE);
                     }
                     return false;
-                    
             }
     }
 
@@ -173,8 +213,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // RGB
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // uint8_t mods_state = get_mods();
-    switch (get_highest_layer(layer_state)) {  // special handling per layer
-        case MODS:                                // on Fn layer select what the encoder does when pressed
+    switch (get_highest_layer(layer_state)) { // special handling per layer
+        case MODS:                            // on Fn layer select what the encoder does when pressed
             highlight_fkeys();
             break;
     }
